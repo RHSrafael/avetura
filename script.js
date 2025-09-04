61b37dcdca3dbd70a68ac0fccca77a2969731a19
RHSrafael <rafael.henrique.soares@escola.pr.gov.br>
2025-09-04 09:05:21 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+const reiniciarBtn = document.getElementById('.bnt-reiciar')
+
+avanca.forEach(button => {
+button.addEventListener('click',function(){
+
+    const atual = document.querySelector('.ativo');
+    const proximoPasso='passo-'+this.getAttribute('data-proximo')
+})
+
+
+}
+
+
+)
